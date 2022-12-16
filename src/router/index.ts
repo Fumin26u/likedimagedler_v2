8eb7c738ad9effe25d7dd9e2fe12f55d9ef2141e
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-12-16 23:52:09 +0900

    Routerを変更

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,23 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import TwiImage from '../views/TwiImage.vue'
+import PixImage from '../views/PixImage.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
+    {
+        path: '/',
+        name: 'twi',
+        component: TwiImage,
+    },
+    {
+        path: '/pix',
+        name: 'pix',
+        component: PixImage,
+    },
+]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+})
 
-export default router;
+export default router
